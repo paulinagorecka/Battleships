37ca38e7081532d5a913de9c460755f0c87381e8
paulinagorecka <pauline.gorecka@gmail.com>
2018-12-19 23:35:58 +0100

    turns added and 'game over' function

--- battleships.py
+++ battleships.py
@@ -2,7 +2,6 @@
 Classic Battleships Game. Enjoy!
 Author: Paulina Gorecka
 """
-
 from random import randint
 
 board = []
@@ -24,24 +23,36 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
 
 for turn in range(4):
-
+	
+  print "Turn %d" %(turn + 1)
   guess_row = int(raw_input("Guess Row: "))
   guess_col = int(raw_input("Guess Col: "))
-
+  
   if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sunk my battleship!"
     
-  else:
+    print "Congratulations! You sunk my battleship!"
+    break  
+    
+  else:  
     if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+      
       print "Oops, that's not even in the ocean."
+      
+    if turn == 3:
+      
+    	print "Game Over"
+      
     elif(board[guess_row][guess_col] == "X"):
+      
       print "You guessed that one already."
+      
     else:
+      
       print "You missed my battleship!"
       board[guess_row][guess_col] = "X"
-    print "Turn %d" %(turn + 1)
     print_board(board)
+    
+    if turn == 3:
+    	print "Game Over"
